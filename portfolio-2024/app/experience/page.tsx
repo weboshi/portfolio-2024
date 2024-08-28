a2cfd12dfd6c0f55dbe68aaf518e53cf9f1a46a6
Le Andrew Nguyen <avident@gmail.com>
2024-08-27 17:12:54 -0700

    up

---
+++ portfolio-2024/app/experience/page.tsx
@@ -0,0 +1,48 @@
+import Image from "next/image";
+
+export default function Home() {
+    return (
+        <main className="items-center">
+            <div className="navbar bg-base-100">
+                <div className="flex-1 justify-between px-10">
+                    <a className="font-bold text-xl">Andy's Simple Portfolio </a>
+                    <ul className="menu menu-vertical lg:menu-horizontal bg-base-200 rounded-box">
+                        <li><a>Experience</a></li>
+                        <li><a>Projects</a></li>
+                        <li><a>Bonus</a></li>
+                    </ul>
+                </div>
+
+
+
+
+
+
+            </div>
+            Experience Page
+            <div className="container mx-auto">
+                <div className="collapse collapse-arrow bg-base-200">
+                    <input type="radio" name="my-accordion-2" defaultChecked />
+                    <div className="collapse-title text-xl font-medium">Click to open this one and close others</div>
+                    <div className="collapse-content">
+                        <p>hello</p>
+                    </div>
+                </div>
+                <div className="collapse collapse-arrow bg-base-200">
+                    <input type="radio" name="my-accordion-2" />
+                    <div className="collapse-title text-xl font-medium">Click to open this one and close others</div>
+                    <div className="collapse-content">
+                        <p>hello</p>
+                    </div>
+                </div>
+                <div className="collapse collapse-arrow bg-base-200">
+                    <input type="radio" name="my-accordion-2" />
+                    <div className="collapse-title text-xl font-medium">Click to open this one and close others</div>
+                    <div className="collapse-content">
+                        <p>hello</p>
+                    </div>
+                </div>
+            </div>
+        </main>
+    );
+}
--- portfolio-2024/app/page.tsx
+++ portfolio-2024/app/page.tsx
@@ -1,20 +1,21 @@
 import Image from "next/image";
+import Link from 'next/link';
+
 
 export default function Home() {
   return (
     <main className="items-center">
       <div className="navbar bg-base-100">
- 
-  <div className="flex-1">
-    <a className="btn btn-ghost text-xl">Andy's Simple Portfolio </a>
-    <ul className="menu menu-vertical lg:menu-horizontal bg-base-200 rounded-box">
-  <li><a>Experience</a></li>
-  <li><a>Projects</a></li>
-  <li><a>Bonus</a></li>
-</ul>
-  </div>
-  
-</div>
+        <div className="flex-1 justify-between px-10">
+          <a className="font-bold text-xl">Andy's Simple Portfolio </a>
+          <ul className="menu menu-vertical lg:menu-horizontal bg-base-200 rounded-box">
+            <li><Link href="/experience/">Experience</Link></li>
+            <li><Link href="/experience/">Projects</Link></li>
+            <li><Link href="/experience/">Bonus</Link></li>
+          </ul>
+        </div>
+
+      </div>
     </main>
   );
 }
