7fbde8b06fdca7ec4481e64cf6f03d07a17cb1c7
Le Andrew Nguyen <avident@gmail.com>
2024-09-13 11:33:16 -0700

    Update page.tsx

--- portfolio-2024/app/experience/page.tsx
+++ portfolio-2024/app/experience/page.tsx
@@ -19,14 +19,14 @@ export default function Home() {
             <div className="container mx-auto">
                 <div className="collapse collapse-arrow bg-base-200">
                     <input type="radio" name="my-accordion-2" defaultChecked />
-                    <div className="collapse-title text-xl font-medium">Click to open this one and close others</div>
+                    <div className="collapse-title text-xl font-medium">Web Developer - Revel Systems </div>
                     <div className="collapse-content">
                         <p>hello</p>
                     </div>
                 </div>
                 <div className="collapse collapse-arrow bg-base-200">
                     <input type="radio" name="my-accordion-2" />
-                    <div className="collapse-title text-xl font-medium">Click to open this one and close others</div>
+                    <div className="collapse-title text-xl font-medium">Web & Software Development Instructor - Zenith Prep Academy </div>
                     <div className="collapse-content">
                         <p>hello</p>
                     </div>
